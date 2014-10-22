aa377201fc0a7d45e11b322e1ca1709820c4b5a9
AceBear <vast_m_bear@yahoo.com>
2014-10-22 13:01:00 +0800

    keep the sensetive data in a separated file

--- test/main.js
+++ test/main.js
@@ -1,12 +1,25 @@
 // test
 
 var Path = require("path");
+var fs = require("fs");
 var AliMQS = require(Path.join(__dirname, "../index.js"));
 
-
 function runTest(){
-    var account = new AliMQS.Account("your-owner-id", "your-key-id", "your-key-secret");
-    var mqs = new AliMQS.MQS(account, "hangzhou");
+    // ali account configuration
+    var aliCfg = {
+        ownerId: "your-owner-id",
+        keyId: "your-key-id",
+        keySecret: "your-key-secret",
+        region: "hangzhou"
+    };
+
+    // test/account.js contains sensitive data, and will not be pushed to github
+    var cfgPath = Path.join(__dirname, "account.js");
+    if(fs.existsSync(cfgPath)){
+        aliCfg = require(cfgPath);
+    }
+    var account = new AliMQS.Account(aliCfg.ownerId, aliCfg.keyId, aliCfg.keySecret);
+    var mqs = new AliMQS.MQS(account, aliCfg.region);
 
     return mqs.listP();
 }
