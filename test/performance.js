24885d78c243b84f9af887a77c144161dc49cdd8
Jeff Tian <jie.tian@unicareer.com>
2019-11-09 13:45:32 +0800

    tests

--- test/performance.js
+++ test/performance.js
@@ -4,7 +4,7 @@ var assert = require("assert");
 var Path = require("path");
 var fs = require("fs");
 var Promise = require("promise");
-var AliMNS = require(Path.join(__dirname, "../index.js"));
+var AliMNS = require(Path.join(__dirname, "../ts/index"));
 var debugTest   = require("debug")("ali-mns.test");
 
 describe.skip('AliMNS-performance', function(){
@@ -26,15 +26,15 @@ describe.skip('AliMNS-performance', function(){
     var account = new AliMNS.Account(aliCfg.accountId, aliCfg.keyId, aliCfg.keySecret);
     var mns = new AliMNS.MNS(account, aliCfg.region);
     var mqs = new Array();
-    
+
     var max_queue = 20;
     var max_msg = 2000;
     var recv_batch_mode = 16;
-    
+
     before(function(done){
         // Make queues
         var mqNameBase = aliCfg.mqName + Math.floor(Math.random() * 10000);
-        
+
         var task = new Array();
         for(var i=1;i<max_queue+1;i++){
             (function(){
@@ -52,7 +52,7 @@ describe.skip('AliMNS-performance', function(){
         }
         Promise.all(task).then(function(task){ done(); }, done);
     });
-    
+
     after(function(done){
         var task = new Array();
         for(var i=0;i<mqs.length;i++){
@@ -72,17 +72,17 @@ describe.skip('AliMNS-performance', function(){
             done();
         }, done);
     });
-    
+
     describe('concurrent-queues', function(){
-        
+
         it('#BatchSend', function(done){
-            
+
             var pickMQ = function(mqs){
                 var idx = Math.floor(Math.random() * mqs.length);
                 if(idx === mqs.length) idx--;
                 return mqs[idx];
             };
-            
+
             var task = [];
             var iSent = 0, batch_size = 16;
             while(iSent < max_msg){
@@ -94,23 +94,23 @@ describe.skip('AliMNS-performance', function(){
                     var mq = pickMQ(mqs);
                     task.push(mq.sendP(msgs));
                     iSent += msgs.length;
-                    
+
                     mq.ccSent += msgs.length;
                     debugTest(tmNow(), mq.getName() + " - " + iSent + " were sent.");
                 })();
             }
             Promise.all(task).then(function(){ done(); });
         });
-        
+
         it('#recvP', function(done){
             var recved = 0;
-            
+
             function countMsg(mq, msg){
                 mq.ccRecv++;
                 recved++;
                 debugTest(tmNow(), recved + " - " + mq.getName() + " - " + msg.MessageBody);
             }
-            
+
             for(var i=0;i<mqs.length;i++){
                 (function(mq){
                     mq.notifyRecv(function(ex, msg){
@@ -128,7 +128,7 @@ describe.skip('AliMNS-performance', function(){
                                     countMsg(mq, msg.Message);
                                 }
                             }
-                            
+
                             if(recved === max_msg) done(); // all messages have been received
                             return true; // auto delete msg
                         }
@@ -139,14 +139,14 @@ describe.skip('AliMNS-performance', function(){
                 })(mqs[i]);
             }
         });
-        
+
         it('#stopRecv', function(done){
             var stopped = 0;
             for(var i=0;i<mqs.length;i++){
                 (function(mq){
                     mq.notifyStopP().then(function(){
                         stopped++;
-                        
+
                         debugTest(tmNow(), mq.getName() + " stopped.");
                         if(stopped === mqs.length) done(); // all mq has stopped.
                     });
@@ -154,7 +154,7 @@ describe.skip('AliMNS-performance', function(){
             }
         });
     });
-    
+
     // help time
     var tmNow = function(){
         var tm = new Date();
