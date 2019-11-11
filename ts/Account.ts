28c07d27f568bea56143bec4ab3c96ac130bbcc5
Jeff Tian <jie.tian@unicareer.com>
2019-11-11 14:17:37 +0800

    fix: tsc errors

--- ts/Account.ts
+++ ts/Account.ts
@@ -1,47 +1,70 @@
 /// <reference path="ali-mns.ts" />
 
 import CryptoA from 'crypto'
-
-
-    // The Ali account, it holds the key id and secret.
-    export class Account{
-        constructor(accountId:string, keyId:string, keySecret:string){
-            this._accountId = accountId;
-            this._keyId = keyId;
-            this._keySecret = keySecret;
-            this._displayName = 'AliAccount'
-        }
-
-        public getAccountId(){ return this._accountId; }
-        public getOwnerId(){ return this._accountId; } // for compatible v1.x
-        public getKeyId(){ return this._keyId; }
-        public getGA(){ return this._bGoogleAnalytics; }
-        public setGA(bGA:boolean){ this._bGoogleAnalytics = bGA; }
-        public getHttps(){ return this._bHttps; }
-        public setHttps(bHttps:boolean){ this._bHttps = bHttps; }
-        public getDisplayName(){return this._displayName;}
-        public setDisplayName(name: string){this._displayName = name;}
-
-        public displayName = 'AliAccount';
-        public id = '';
-        public rpDisplayName = 'AliAccount';
-
-        // encoding: "hex", "binary" or "base64"
-        public hmac_sha1(text:string, encoding?:string){
-            var hmacSHA1:any = CryptoA.createHmac("sha1", this._keySecret);
-            return hmacSHA1.update(text).digest(encoding);
-        }
-
-        public b64md5(text:string){
-            var cryptoMD5 = CryptoA.createHash("md5");
-            return cryptoMD5.update(new Buffer(text, 'utf-8')).digest("base64");
-        }
-
-        private _accountId: string; // Owner id
-        private _keyId: string; // Access key id
-        private _keySecret: string; // Access key secret
-        private _bGoogleAnalytics = true; // Enable Google Analytics
-        private _bHttps = false; // Default to use http
-        private _displayName = ''
+// The Ali account, it holds the key id and secret.
+export class Account {
+    constructor(accountId: string, keyId: string, keySecret: string) {
+        this._accountId = accountId;
+        this._keyId = keyId;
+        this._keySecret = keySecret;
+        this._displayName = 'AliAccount'
     }
+
+    public getAccountId() {
+        return this._accountId;
+    }
+
+    public getOwnerId() {
+        return this._accountId;
+    } // for compatible v1.x
+    public getKeyId() {
+        return this._keyId;
+    }
+
+    public getGA() {
+        return this._bGoogleAnalytics;
+    }
+
+    public setGA(bGA: boolean) {
+        this._bGoogleAnalytics = bGA;
+    }
+
+    public getHttps() {
+        return this._bHttps;
+    }
+
+    public setHttps(bHttps: boolean) {
+        this._bHttps = bHttps;
+    }
+
+    public getDisplayName() {
+        return this._displayName;
+    }
+
+    public setDisplayName(name: string) {
+        this._displayName = name;
+    }
+
+    public displayName = 'AliAccount';
+    public id = '';
+    public rpDisplayName = 'AliAccount';
+
+    // encoding: "hex", "binary" or "base64"
+    public hmac_sha1(text: string, encoding?: string) {
+        var hmacSHA1: any = CryptoA.createHmac("sha1", this._keySecret);
+        return hmacSHA1.update(text).digest(encoding);
+    }
+
+    public b64md5(text: string) {
+        var cryptoMD5 = CryptoA.createHash("md5");
+        return cryptoMD5.update(new Buffer(text, 'utf-8')).digest("base64");
+    }
+
+    private _accountId: string; // Owner id
+    private _keyId: string; // Access key id
+    private _keySecret: string; // Access key secret
+    private _bGoogleAnalytics = true; // Enable Google Analytics
+    private _bHttps = false; // Default to use http
+    private _displayName = ''
+}
 
